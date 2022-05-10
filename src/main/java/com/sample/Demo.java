de06e718a9bfbed923367128dfccf3179980d045
Ravindra Garapati <Ravindra Garapati@Ravindra-Garapati>
2022-05-10 12:24:51 +0530

    Removed the space in Demo class

--- src/main/java/com/sample/Demo.java
+++ src/main/java/com/sample/Demo.java
@@ -4,8 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Selenium Tutorials");
-		
+		System.out.println("Welcome to Selenium Tutorials");	
 		System.out.println("Updated the code");
 
 	}
